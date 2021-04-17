bf241f4521a7487cc610fd8d56f26f8d77d907d0
Alana <alanahoang@yahoo.com>
2021-04-17 16:41:50 -0700

    added css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-console.log("Hi! Welcome to My Portfolio Site! Feel free to look around. ^-^")
-
-onclick="myFunction()"
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
